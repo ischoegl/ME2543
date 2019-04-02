aa38d1e5caff594a6fc845d464d06ebc86a72b2b
Vinicius - prodesk <vsauer@gmail.com>
2019-04-02 13:29:53 -0500

    integration

---
+++ integration/trapezoidal.m
@@ -0,0 +1,22 @@
+function int = trapezoidal(func, a, b, n, varargin)
+    % trap: composite trapezoidal rule quadrature
+    % int = trap(func, a, b, n):
+    % Composite trapezoidal rule
+    % input:
+    %   func = name of function to be integrated
+    %   a, b = integration limits
+    %   n = number of segments
+    % output:
+    %   I = integral estimate
+    if ~(b>a)
+        error('Upper bound must be greater than lower')
+    end
+    x = a;
+    h = (b - a)/n;
+    s = func(a);
+    for i = 1:n-1
+        x = x + h;
+        s = s + 2 * func(x);
+    end
+    s = s + func(b);
+    int = (b - a) * s / (2 * n);
